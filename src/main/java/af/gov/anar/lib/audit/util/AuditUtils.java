44bc10c53a6ad11ddb8067d38748d72a25b043d1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-14 10:21:27 +0430

    utility functions

---
+++ src/main/java/af/gov/anar/lib/audit/util/AuditUtils.java
@@ -0,0 +1,46 @@
+package af.gov.anar.lib.audit.util;
+
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+/**
+ * Utility class for Audit Manager
+ *
+ */
+public class AuditUtils {
+
+	/**
+	 * Private constructor for AuditUtils
+	 */
+	private AuditUtils() {
+
+	}
+
+	/**
+	 * Function to validate {@link AuditRequestDto}
+	 * 
+	 * @param auditRequest The audit request
+	 */
+	public static void validateAuditRequest(AuditRequestDto auditRequest) {
+		ValidatorFactory factory = null;
+		try {
+			factory = Validation.buildDefaultValidatorFactory();
+			Validator validator = factory.getValidator();
+
+			Set<ConstraintViolation<AuditRequestDto>> violations = validator.validate(auditRequest);
+
+			if (!violations.isEmpty()) {
+				throw new AuditManagerException(AuditErrorCodes.HANDLEREXCEPTION.getErrorCode(),
+						AuditErrorCodes.HANDLEREXCEPTION.getErrorMessage());
+			}
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+	}
+}
