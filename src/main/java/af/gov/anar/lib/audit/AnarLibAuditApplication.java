905855b382861b3d77075d33cb0c803fbfed15a3
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-14 10:10:28 +0430

    audit utility library

---
+++ src/main/java/af/gov/anar/lib/audit/AnarLibAuditApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lib/audit/AnarLibAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
