93f17fa137188e5b062bcb583945d93c7e13a836
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-14 14:32:16 +0430

    data model

---
+++ src/main/java/af/gov/anar/lib/audit/repository/AuditRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package af.gov.anar.lib.audit.repository;
+
+import af.asr.auditmanager.entity.Audit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository interface with data access and data modification functions on
+ * {@link Audit}
+ */
+public interface AuditRepository extends JpaRepository<Audit, Long> {
+
+}
